104d33a4751f854326e74421d1ca628d3c829acc
Ben Chapman <bchapman@engineyard.com>
2012-11-12 21:01:39 +0000

    Got rid of date helper functions, replace with moment.js entirely

--- js/controllers/dcuttViewCtrl.js
+++ js/controllers/dcuttViewCtrl.js
@@ -1,35 +1,10 @@
 'use strict';
 
 dcutt.controller( 'dcuttViewCtrl', function( $scope, $location, $routeParams, $http) {
-  
-  /** 
-   * Helper functions for handling dates some devices *cough*iOS*cough*
-   * decided to not support yyyy-mm-dd as a valid date
-   */
-  
-  var dateToString = function(date) {
-    return moment(date).format("YYYY-MM-DD");
-  }
     
-  var stringToDate = function(dateString) {
-    return moment(dateString, "YYYY-MM-DD");
-  }
-    
-  var getTomorrowsDate = function(dateString) {
-    return moment(dateString, "YYYY-MM-DD").add('d',1).format("YYYY-MM-DD");
-  }
-    
-  var getYesterdaysDate = function(dateString) {
-    return moment(dateString, "YYYY-MM-DD").subtract('d',1).format("YYYY-MM-DD");
-  }
-  
-  var getDayName = function(dateString) {
-    return moment(dateString, "YYYY-MM-DD").format("dddd");
-  }
-  
-  $scope.date = dateToString(new Date());
+  $scope.date = moment(date).format("YYYY-MM-DD");
   var timetable = {}
-  
+
   $http({method: 'GET', url: 'https://api.dcutt.com/index.php?coursecode=' + $routeParams.coursecode}).
     success(function(data) {
       timetable = data;
@@ -38,22 +13,22 @@ dcutt.controller( 'dcuttViewCtrl', function( $scope, $location, $routeParams, $h
     error(function() {
       $location("/");
     });
-    
+
   var updateEvents = function() {
     $scope.day = timetable[$scope.date];
     $scope.dayDate = $scope.date;
-    $scope.dayName = getDayName($scope.date);
+    $scope.dayName = moment($scope.date, "YYYY-MM-DD").format("dddd");
   }
   $scope.updateEvents = updateEvents;
-    
+
   var nextDay = function() {
-    $scope.date = getTomorrowsDate($scope.date);
+    $scope.date = moment($scope.date, "YYYY-MM-DD").add('d',1).format("YYYY-MM-DD");
     updateEvents();
   }
   $scope.nextDay = nextDay;
-    
+
   var prevDay = function() {
-    $scope.date = getYesterdaysDate($scope.date);
+    $scope.date = moment($scope.date, "YYYY-MM-DD").subtract('d',1).format("YYYY-MM-DD");
     updateEvents();
   }
   $scope.prevDay = prevDay;
