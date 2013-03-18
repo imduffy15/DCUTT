0c11524464be3263431fb4ea1b1f36000c61585a
Ian Duffy <ian@ianduffy.ie>
2013-03-18 14:29:41 +0000

    Add horrible temp fix for scrolling issues

--- app/scripts/controllers/error.js
+++
@@ -1,9 +0,0 @@
-dcuTimetablesApp.controller('ErrorCtrl',
-  ['$location', '$routeParams', '$scope',
-    function($location, $routeParams, $scope) {
-      alert("Error processing your request!");
-      storage.clear();
-      $location.path('');
-    }
-  ]
-);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,8 @@
 dcuTimetablesApp.controller('MainCtrl',
   ['$location', '$routeParams', '$scope',
     function($location, $routeParams, $scope) {
-
       var coursecode = storage.getItem('coursecode') || false;
-
+      
       if (coursecode) {
           $location.path(coursecode);
       }
